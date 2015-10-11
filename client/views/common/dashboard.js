5a35b748a9cbe4783874bb86b461149de6e12236
Brock Lanoza <brocklanoza@gmail.com>
2015-10-10 21:29:58 -1000

    Integrated nvd3 scatter chart

--- client/views/common/dashboard.js
+++ client/views/common/dashboard.js
@@ -1,22 +1,5 @@
 Template.dashboardLayout.rendered = function() {
-nv.addGraph(function() {
-  var chart = nv.models.scatterChart()
-                .showDistX(true)
-                .showDistY(true)
-                .color(d3.scale.category10().range());
 
-  chart.xAxis.tickFormat(d3.format('.02f'));
-  chart.yAxis.tickFormat(d3.format('.02f'));
-
-  d3.select('#chart svg')
-      .datum(data(4,40))
-    .transition().duration(500)
-      .call(chart);
-
-  nv.utils.windowResize(chart.update);
-
-  return chart;
-});
 }
 
 Template.dashboardLayout.created = function() {
@@ -35,19 +18,50 @@ Template.dashboardLayout.events({
 });
 
 
-function (groups, points) {
-  var data = [],
+//main chart template that contains the primary data
+Template.mainChart.rendered = function() {
+  nv.addGraph(function() {
+    var chart = nv.models.scatterChart()
+                  .showDistX(true)
+                  .showDistY(true)
+                  .color(d3.scale.category10().range());
+
+    chart.xAxis.tickFormat(d3.format('.02f'));
+    chart.yAxis.tickFormat(d3.format('.02f'));
+
+    d3.select('#chart svg')
+        .datum(data(4,40))
+      .transition().duration(500)
+        .call(chart);
+
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+});
+
+}
+
+Template.mainChart.created = function() {
+
+}
+
+Template.mainChart.destroyed = function() {
+
+}
+
+var data = (function (groups, points) {
+      randomArray = [],
       shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'],
       random = d3.random.normal();
 
   for (i = 0; i < groups; i++) {
-    data.push({
-      key: 'Group ' + i,
+    randomArray.push({
+      key: 'Position' + i,
       values: []
     });
 
     for (j = 0; j < points; j++) {
-      data[i].values.push({
+      randomArray[i].values.push({
         x: random()
       , y: random()
       , size: Math.random()
@@ -56,5 +70,5 @@ function (groups, points) {
     }
   }
 
-  return data;
-}
+  return randomArray;
+});
